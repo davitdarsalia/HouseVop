1f381688b24cf2809531e1f3267382a94204af62
Davit Darsalia <79382138+davitdarsalia@users.noreply.github.com>
2021-05-13 22:05:56 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+jQuery(function() {
+
+    // First
+
+   let increment = 1;
+
+   while(increment < 3000) {
+       $('.main__header').fadeOut(1500);
+       $('.main__header').fadeIn(1500);
+
+       increment++;
+   }
+
+    // Second
+
+   $('.menu__item').hover(function() {
+       
+   })
+
+})
